128824e01d99d1eb2e5a8bc81da796efe17dee28
Edward Klimoshenko <39220927+eklimo@users.noreply.github.com>
2023-03-06 03:00:45 -0500

    `app-server`: Add health check

--- app-server/src/main/kotlin/com/eklimo/dynamicplaylist/DynamicPlaylistApplication.kt
+++ app-server/src/main/kotlin/com/eklimo/dynamicplaylist/DynamicPlaylistApplication.kt
@@ -2,8 +2,17 @@ package com.eklimo.dynamicplaylist
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication class DynamicPlaylistApplication
+
+@RestController
+class HealthController {
+  /** Returns a `200 OK` response, indicating that the server is healthy. */
+  @GetMapping("/health") fun healthCheck() = ResponseEntity.ok().build<Nothing>()
+}
 
 fun main(args: Array<String>) {
   runApplication<DynamicPlaylistApplication>(*args)
